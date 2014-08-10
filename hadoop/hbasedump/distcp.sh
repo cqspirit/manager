1e2e9a31e721a1beda9590d978b1b2e18b0bbc31
spirit <jnspirit@163.com>
2014-08-11 00:04:07 +0800

    Update distcp.sh

--- hadoop/hbasedump/distcp.sh
+++ hadoop/hbasedump/distcp.sh
@@ -18,8 +18,8 @@ function distcp(){
   currTime=$(date +%Y%m%d-%H%M) #string of date time
   echo "*********************************************$currTime***********************************************************************" 	>>$logfile
   echo "start  distcp $1 from $src to $des "  											   	>>$logfile
-  echo "command is:hadoop distcp hftp://$src:50070/export/modfiy/$1  webhdfs://$des:50070/move/import/$1				     "	>>$logfile
-  hadoop distcp hftp://$src:50070/move/import/$1  webhdfs://$des:50070/move/import/$1 
+  echo "command is:hadoop distcp -update -skipcrccheck hftp://$src:50070/export/modfiy/$1  webhdfs://$des:50070/move/import/$1				     "	>>$logfile
+  hadoop distcp -update -skipcrccheck hftp://$src:50070/move/import/$1  webhdfs://$des:50070/move/import/$1 
   echo "finish distcp table:$1" 													>>$logfile
   echo "result is $?"															>>$logfile
   echo "*****************************************************************************************************************************" 	>>$logfile
