bc0c12bd84830778f1d697b95bf23e857233c48c
spirit <jnspirit@163.com>
2014-08-11 11:47:30 +0800

    Create check2RestartRegionServer.sh

---
+++ hadoop/check2RestartRegionServer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#add by geyong Aug 11 2014
+#description: check regionserver status,if it is down,restart it
+
+export logfile="/var/log/regionserver-status-$(date +%Y%m%d).log"
+
+if [ -f $logfile ]; then
+  echo "$logifle exists!"
+else
+  touch $logfile
+fi
+
+PS="/bin/ps"
+GREP="/bin/grep"
+
+ret=`$PS auxwwwf |$GREP java|$GREP HRegionServer|wc -l `
+
+if [[ "$ret" -ge "1" ]]; then
+     echo "[$(date +%Y%m%d-%H%M)]:OK  RegionServer is running!">>$logfile
+else
+     echo "[$(date +%Y%m%d-%H%M)]:CRITICAL!! RegionServer is down!">>$logfile
+     echo "start RegionServer..."
+     su - hbase -c "/usr/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf start regionserver"
+     if [[ "$?" -ne "1" ]]; then
+        echo "start RegionServer successed..."
+     else
+        echo "start RegionServer failed..."
+     fi
+fi
