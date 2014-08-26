733ca07817157830292d28e9c861ba23fef17a35
spirit <jnspirit@163.com>
2014-08-26 21:39:28 +0800

    Create update_code.sh

---
+++ crawler/update_code.sh
@@ -0,0 +1,41 @@
+
+# setup users
+set +e
+useradd dc-agent -M -p $PASSWORD
+set -e
+
+BASE_DIR=/opt/crawl
+AGENT_DIR=$BASE_DIR/agent
+MANAGER_DIR=$BASE_DIR/manager
+
+rm -rf $BASE_DIR
+mkdir -p $BASE_DIR
+
+cd $BASE_DIR
+# Setup Manager
+git clone https://github.com/xxr3376/Distribute-Crawler-Manager $MANAGER_DIR
+curl https://raw.githubusercontent.com/cqspirit/manager/master/crawler/config.py > $MANAGER_DIR/config.py
+
+chown -R dc-agent:dc-agent $MANAGER_DIR
+
+
+cd $MANAGER_DIR
+virtualenv .env
+. .env/bin/activate
+pip install -r requirement.txt
+
+
+cd $BASE_DIR
+# Setup Agent
+git clone https://github.com/xxr3376/Distribute-Crawler-Agent $AGENT_DIR
+curl https://raw.githubusercontent.com/cqspirit/manager/master/crawler/const.py > $AGENT_DIR/const.py
+
+chown -R dc-agent:dc-agent $AGENT_DIR
+
+cd $AGENT_DIR
+virtualenv .env
+. .env/bin/activate
+yum install libxslt-devel -y
+pip install -r requirement.txt
+
+supervisorctl reload
