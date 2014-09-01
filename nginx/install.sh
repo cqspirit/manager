1e2f72c8d1d3e601bc62add070cb3200ba3fdbb6
spirit <jnspirit@163.com>
2014-08-31 19:54:33 -0700

    Create install.sh

---
+++ nginx/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#install nginx add By geyong 1 Sep 2014
+#模块依赖性
+yum -y install gcc gcc-c++ autoconf automake
+#gzip模块需要zlib库
+#rewrite模块需要pcre库
+#ssl功能需要openssl库等
+yum -y install zlib zlib-devel openssl openssl-devel pcre pcre-devel
+#get latest nginx version
+mkdir -p /opt/soft
+cd /opt/soft
+wget http://nginx.org/download/nginx-1.6.1.tar.gz
+tar zxvf nginx-1.6.1.tar.gz
+cd nginx-1.6.1
+./configure
+make
+make install
