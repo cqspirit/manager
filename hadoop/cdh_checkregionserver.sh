6aed66429ab64a643706736128a1dbaf35d376ce
spirit <jnspirit@163.com>
2014-09-25 14:09:30 +0800

    cdh_checkregionserver.sh

---
+++ hadoop/cdh_checkregionserver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#add by geyong Aug 11 2014
+#description: check regionserver status,if it is down,restart it
+
+export logfile="/var/log/regionserver-status-$(date +%Y%m%d).log"
+
+if [ -f $logfile ]; then
+  echo "$logifle exists!"
+else
+  touch $logfile
+fi
+
+PS="/bin/ps"
+GREP="/bin/grep"
+
+ret=`$PS auxwwwf |$GREP java|$GREP HRegionServer|wc -l `
+
+if [[ "$ret" -ge "1" ]]; then
+     echo "[$(date +%Y-%m-%d' '%H:%M)]:OK!  RegionServer is running!">>$logfile
+else
+     echo "[$(date +%Y-%m-%d' '%H:%M)]:CRITICAL!! RegionServer is down!">>$logfile
+     echo "[$(date +%Y-%m-%d' '%H:%M)]:start RegionServer........">>$logfile
+     su - hbase -c "/opt/cloudera/parcels/CDH-4.7.0-1.cdh4.7.0.p0.40/lib/hbase/bin/hbase-daemon.sh --config /etc/hbase/conf start regionserver"
+     if [[ "$?" -ne "1" ]]; then
+        echo "[$(date +%Y-%m-%d' '%H:%M)]:start RegionServer successed!!">>$logfile
+     else
+        echo "[$(date +%Y-%m-%d' '%H:%M)]:start RegionServer failed!!!">>$logfile
+     fi
+fi
