8915f4e9e7a3d9be8a5d1d652a6a430072fc85b1
spirit <jnspirit@163.com>
2015-01-16 10:59:46 +0800

    Update setup_crawler_client.sh

--- crawler/setup_crawler_client.sh
+++ crawler/setup_crawler_client.sh
@@ -9,7 +9,8 @@ yum groupinstall -y development
 yum install -y zlib-dev openssl-devel sqlite-devel bzip2-devel wget
 yum install xz-libs -y
 
-wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz -o Python-2.7.6.tar.xz
+wget http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz 
+#-o Python-2.7.6.tar.xz
 xz -d Python-2.7.6.tar.xz
 tar -xvf Python-2.7.6.tar
 cd Python-2.7.6
